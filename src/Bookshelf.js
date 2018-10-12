531bf5b4d7bf63736c0d625420ab7a0476acdd47
Adam Robinson <ajrobin@me.com>
2018-10-11 19:57:58 -0500

    Finish the functionality of the project to meet the requirements of the rubric

---
+++ src/Bookshelf.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+
+class Bookshelf extends Component {
+	state = {
+		shelves: [
+			{id:'currentlyReading', text: 'Currently Reading'},
+			{id:'wantToRead', text:'Want to Read'},
+			{id:'read', text: 'Read'} 
+		]
+	}
+
+	componentDidUpdate(prevProps){
+		if(prevProps === this.props){
+			console.log('Updated')
+		}
+
+	}
+
+	thumbnail = (book) => {
+		if(!book.hasOwnProperty('imageLinks')){
+			return { width: 128, height: 192, backgroundImage: `url('')`}
+		}else{
+			return { width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail})`}
+		}
+	}
+
+	render(){
+		return(
+			<div className="list-books">
+	            <div className="list-books-title">
+	              <h1>MyReads</h1>
+	            </div>
+	            <div className="list-books-content">
+	              <div>
+	              	{this.state.shelves.map((category) =>
+						<div className="bookshelf" key={category.id}>
+			                <h2 className="bookshelf-title">{category.text}</h2>
+			                <div className="bookshelf-books">
+			                    <ol className="books-grid">
+			                      	{this.props.books !== undefined && Array.isArray(this.props.books) && (
+							    		this.props.books.filter((book) => book.shelf === category.id).map((book) => 
+								    		<li key={book.id}>
+						                    	<div className="book">
+						                        	<div className="book-top">
+						                            	<div className="book-cover" style={this.thumbnail(book)}></div>
+						                            	<div className="book-shelf-changer">
+						                              		<select value={category.id} onChange={((e) => this.props.updateLibrary(book, e))}>
+						                                		<option value="move" disabled>Move to...</option>
+						                               			<option value="currentlyReading">Currently Reading</option>
+						                                		<option value="wantToRead">Want to Read</option>
+						                                		<option value="read">Read</option>
+						                                		<option value="none">None</option>
+						                              		</select>
+						                            	</div>
+						                          	</div>
+						                          	<div className="book-title">{book.title}</div>
+		                          					<div className="book-authors">{book.authors}</div>
+						                        </div>
+						                    </li>
+						                )
+							    	)}
+			                    </ol>
+			                </div>
+		                </div>
+					)}
+	              </div>
+	            </div>
+	            <div className="open-search">
+	              <Link to='/search'>Add a book</Link>
+	            </div>
+          	</div>
+		)
+	}
+}
+
+export default Bookshelf
---
+++ src/SearchBooks.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+
+class SearchBooks extends Component {
+
+	thumbnail = (book) => {
+		if(!book.hasOwnProperty('imageLinks')){
+			return { width: 128, height: 192, backgroundImage: `url('../public/missingBook.jpg')`}
+		}else{
+			return { width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail})`}
+		}
+	}
+
+	onShelfAlready(book){
+		let category = ''
+		this.props.books.filter(checkBook => checkBook.id === book.id).map(checkBook => category = checkBook.shelf)
+		if(category === ''){
+			category = 'none'
+		}
+		return category
+	}
+
+	render(){
+		return(
+			<div className="search-books">
+			  <div className="search-books-bar">
+			    <Link className='close-search' to='/'>Close</Link>
+			    <div className="search-books-input-wrapper">
+			      {/*
+			        NOTES: The search from BooksAPI is limited to a particular set of search terms.
+			        You can find these search terms here:
+			        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+			        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+			        you don't find a specific author or title. Every search is limited by search terms.
+			      */}
+			      <input type="text" placeholder="Search by title or author" onChange={(e) => this.props.searchUpdate(e)}/>
+			    </div>
+			  </div>
+			  <div className="search-books-results">
+			    <ol className="books-grid">
+			    	{this.props.currentResults !== undefined && Array.isArray(this.props.currentResults) && (
+			    		this.props.currentResults.map((book) => 
+							<li key={book.id}>
+		                    	<div className="book">
+		                        	<div className="book-top">
+		                            	<div className="book-cover" style={this.thumbnail(book)}></div>
+		                            	<div className="book-shelf-changer">
+		                              		<select value={this.onShelfAlready(book)} onChange={((e) => this.props.updateLibrary(book, e))}>
+		                                		<option value="move" disabled>Move to...</option>
+		                               			<option value="currentlyReading">Currently Reading</option>
+		                                		<option value="wantToRead">Want to Read</option>
+		                                		<option value="read">Read</option>
+		                                		<option value="none">None</option>
+		                              		</select>
+		                            	</div>
+		                          	</div>
+		                          	<div className="book-title">{book.title}</div>
+		                          	<div className="book-authors">{book.authors}</div>
+		                        </div>
+		                    </li>
+		                )
+			    	)}
+			    </ol>
+
+			  </div>
+			</div>
+		)
+	}
+}
+
+export default SearchBooks
